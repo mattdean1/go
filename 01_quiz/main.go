a06e3ab0dd35237f790f8eea54f2e858243ecc1c
mattdean1 <matt.dean.001@gmail.com>
2018-11-04 16:34:30 +0100

    Add basic timer to quiz

--- 01_quiz/main.go
+++ 01_quiz/main.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"time"
 )
 
 type Problem struct {
@@ -14,21 +15,42 @@ type Problem struct {
 
 func main() {
 	csvFilename := flag.String("csv", "problems.csv", "a csv file in the format 'question,answer', e.g. '1+1,2'")
+	timeLimit := flag.Int("time", 30, "the time limit in seconds")
 	flag.Parse()
 
 	problems := readCsv(*csvFilename)
+	timer := time.NewTimer(time.Duration(*timeLimit) * time.Second)
 
 	correctAnswers := 0
 	for i, problem := range problems {
-		fmt.Printf("Problem %d: %s = \n", i+1, problem.question)
-		var answer string
-		fmt.Scanf("%s\n", &answer)
-		if answer == problem.answer {
-			correctAnswers++
+		select {
+		case <-timer.C:
+			endQuiz(correctAnswers, len(problems))
+		default:
+			fmt.Printf("Problem %d: %s = \n", i+1, problem.question)
+			var answer string
+			fmt.Scanf("%s\n", &answer)
+			if answer == problem.answer {
+				correctAnswers++
+			}
 		}
 	}
+	endQuiz(correctAnswers, len(problems))
+}
 
-	fmt.Printf("You scored %d out of %d \n", correctAnswers, len(problems))
+func readCsv(filename string) []Problem {
+	file, err := os.Open(filename)
+	if err != nil {
+		exit(1, fmt.Sprintf("Failed to open csv file %s", filename))
+	}
+
+	r := csv.NewReader(file)
+	lines, err := r.ReadAll()
+	if err != nil {
+		exit(1, "Failed to parse csv")
+	}
+
+	return parseLines(lines)
 }
 
 func parseLines(lines [][]string) []Problem {
@@ -42,22 +64,11 @@ func parseLines(lines [][]string) []Problem {
 	return problems
 }
 
-func readCsv(filename string) []Problem {
-	file, err := os.Open(filename)
-	if err != nil {
-		exit(fmt.Sprintf("Failed to open csv file %s", filename))
-	}
-
-	r := csv.NewReader(file)
-	lines, err := r.ReadAll()
-	if err != nil {
-		exit("Failed to parse csv")
-	}
-
-	return parseLines(lines)
+func endQuiz(score, total int) {
+	exit(0, fmt.Sprintf("You scored %d out of %d \n", score, total))
 }
 
-func exit(msg string) {
+func exit(code int, msg string) {
 	fmt.Println(msg)
 	os.Exit(1)
 }
